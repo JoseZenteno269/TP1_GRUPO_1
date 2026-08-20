8a49735992261c61e890d51b2ea6409cc0df7dbc
LucasJuarezDev <lucasmartinjuarez5@gmail.com>
2026-08-20 11:09:59 -0300

    add principal main and create two object

---
+++ TP1_GRUPO_1_v1/src/Dominio/Principal.java
@@ -0,0 +1,11 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Empleado emp1 = new Empleado();
+		Empleado emp2 = new Empleado();
+
+	}
+
+}
